941d68a30d9ed48753e9d7fef640745c22a5d571
Kwantux <git@kwantux.de>
2025-05-19 22:53:38 +0200

    Fixed neighbour calculation for the water

--- src/functions/waterbyheight/Functions.ts
+++ src/functions/waterbyheight/Functions.ts
@@ -9,11 +9,11 @@ export function waterByHeight(heightMap: Float32Array, relativeHeight: number): 
 
   const waterLevel = min + (max-min) * relativeHeight;
   for (let i = 0; i < heightMap.length; i++) {
-    const neighbours = [i, i+1, i-1, i+SEGMENTS, i-SEGMENTS, i-SEGMENTS-1, i-SEGMENTS+1, i+SEGMENTS-1, i+SEGMENTS+1];
+    const neighbours = [i, i+1, i-1, i+SEGMENTS, i-SEGMENTS, i-SEGMENTS-1, i-SEGMENTS+1, i+SEGMENTS-1, i+SEGMENTS+1, i+2*SEGMENTS, i-2*SEGMENTS, i+2*SEGMENTS+1, i-2*SEGMENTS+1, i+2*SEGMENTS-1, i-2*SEGMENTS-1, i+2, i-2, i+SEGMENTS+2, i-SEGMENTS+2, i+SEGMENTS-2, i-SEGMENTS-2, i+SEGMENTS*2, i-SEGMENTS*2, i+SEGMENTS*2+1, i-SEGMENTS*2+1, i+SEGMENTS*2-1, i-SEGMENTS*2-1];
     let visible = false;
-    for (let i = 0; i < 9; i++) {
-      if (neighbours[i] < heightMap.length && neighbours[i] > 0)
-        if (heightMap[i] > heightMap[neighbours[i]]) visible = true;
+    for (let j = 0; j < neighbours.length; j++) {
+      if (neighbours[j] < heightMap.length && neighbours[j] > 0)
+        if (waterLevel >= heightMap[neighbours[j]]) visible = true;
     }
     data[i] = visible ? waterLevel : Infinity;
   }
