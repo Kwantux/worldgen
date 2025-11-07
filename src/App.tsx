0198dd57bc39dfbe1f8b46901017361aee2769fe
Kwantux <git@kwantux.de>
2025-11-07 07:39:55 +0100

    Remove unused button for showing maps

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div className="bg-gray-900" style={{ display: 'flex', flexDirection: 'row', height: '100vh', color: 'white'}}>
-      <button 
+      {/* <button 
         onClick={() => setShowRightSidebar(!showRightSidebar)}
         style={{
           position: 'absolute',
@@ -36,7 +36,7 @@ const App = () => {
         }}
       >
         {showRightSidebar ? 'Hide Maps' : 'Show Maps'}
-      </button>
+      </button> */}
       <div className="overflow-y-auto" style={{ flex: "0 0 260px", backgroundColor: '#080808', padding: '10px'}}>
         {
           // Left sidebar
